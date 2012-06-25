fd21531cf57addb2fa01a60e1fd3be83fd2542d6
srcspider <source.spider@gmail.com>
2012-06-25 18:13:40 +0300

    update++

--- +App/themes/ibidem/backend/common/base.php
+++ +App/themes/ibidem/backend/common/base.php
@@ -9,7 +9,7 @@
 	$base_url = '//'.$base_config['domain'].$base_config['path'].$base_config['site:frontend'];
 ?>
 
-<div class="navbar" style="padding: 5px">
+<div class="navbar navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container">
 			<a class="brand" href="<?= $base_url ?>"><?= $site_title ?></a>
@@ -20,38 +20,42 @@
 	</div>
 </div>
 
-<div class="row">
+<div class="container">
 	
-	<nav role="navigation" style="padding: 0 10px" class="span2">
+	<div class="row">
 
-		<? $page_slug = $control->pageslug() ?>
-		
-		<ul class="nav nav-list well">
-			<? foreach ($context->dashboard() as $group): ?>
-				<li class="nav-header"><strong><?= $group['title'] ?></strong></li>
-				<? foreach ($group['tools'] as $tool): ?>
-					<li<?= $page_slug == $tool['slug'] ? ' class="active"' : '' ?>>
-						<a href="<?= $tool['url'] ?>">
-							<? if ($tool['icon'] !== null): ?>
-								<i class="icon-<?= $tool['icon'] ?>"></i>
-							<? endif; ?>
-							<?= $tool['title'] ?>
-						</a>
-					</li>
+		<nav role="navigation" class="span2">
+
+			<? $page_slug = $control->pageslug() ?>
+
+			<ul class="nav nav-list well">
+				<? foreach ($context->dashboard() as $group): ?>
+					<li class="nav-header"><strong><?= $group['title'] ?></strong></li>
+					<? foreach ($group['tools'] as $tool): ?>
+						<li<?= $page_slug == $tool['slug'] ? ' class="active"' : '' ?>>
+							<a href="<?= $tool['url'] ?>">
+								<? if ($tool['icon'] !== null): ?>
+									<i class="icon-<?= $tool['icon'] ?>"></i>
+								<? endif; ?>
+								<?= $tool['title'] ?>
+							</a>
+						</li>
+					<? endforeach; ?>
 				<? endforeach; ?>
-			<? endforeach; ?>
-		</ul>
+			</ul>
 
-	</nav>
+		</nav>
 
-	<section id="page" class="span10">
+		<section id="page" class="span10">
 
-		<noscript>
-			<p class="alert"><strong>Hi!</strong> Please whitelist script(s) from this domain. Otherwise, site functions may not work, or will function inadequetly.</p>
-		</noscript>
-		
-		<?= $view->render() ?>
+			<noscript>
+				<p class="alert"><strong>Hi!</strong> Please whitelist script(s) from this domain. Otherwise, site functions may not work, or will function inadequetly.</p>
+			</noscript>
 
-	</section>
+			<?= $view->render() ?>
+
+		</section>
+
+	</div>
 	
 </div>
