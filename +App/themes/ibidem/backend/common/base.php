e363462c32573a0bfeab57e5dcb15cf8fe792b15
srcspider <source.spider@gmail.com>
2012-06-26 01:49:29 +0300

    backend styling++

--- +App/themes/ibidem/backend/common/base.php
+++ +App/themes/ibidem/backend/common/base.php
@@ -8,23 +8,23 @@
 	$site_title = $base_config['site:title'];
 	$base_url = '//'.$base_config['domain'].$base_config['path'].$base_config['site:frontend'];
 ?>
+	
+<div class="container">
 
-<div class="navbar navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="brand" href="<?= $base_url ?>"><?= $site_title ?></a>
-			<ul class="nav">
-				<li><a href="<?= \app\Relay::route('\ibidem\backend')->url() ?>"><i class="icon-cogs"></i> System Information</a>
-			</ul>
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="<?= $base_url ?>"><?= $site_title ?></a>
+				<ul class="nav">
+					<li><a href="<?= \app\Relay::route('\ibidem\backend')->url() ?>"><i class="icon-cogs"></i> System Information</a>
+				</ul>
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="container">
-	
 	<div class="row">
 
-		<nav role="navigation" class="span2">
+		<nav role="navigation" class="span3">
 
 			<? $page_slug = $control->pageslug() ?>
 
@@ -46,16 +46,27 @@
 
 		</nav>
 
-		<section id="page" class="span10">
+		<section id="page" class="span9">
 
 			<noscript>
-				<p class="alert"><strong>Hi!</strong> Please whitelist script(s) from this domain. Otherwise, site functions may not work, or will function inadequetly.</p>
+				<p class="alert">
+					<strong>Hi!</strong> Please whitelist script(s) from this domain. Otherwise, site functions may not work, or will function inadequetly.
+				</p>
 			</noscript>
+
+			<? if (Access::can('\ibidem\backend', null, null, \app\A12n::guest())): ?>
+				<p class="alert alert-error">
+					<button data-dismiss="alert" class="close" type="button">×</button>
+					<big><strong>Major security vulnerability detected</strong>: guest role has access to backend systems!</big>
+					<br/>
+					If you are viewing this in a production environment <strong>drop all you're doing and fix it NOW</strong>!
+				</p>
+			<? endif; ?>
 
 			<?= $view->render() ?>
 
 		</section>
 
 	</div>
-	
+
 </div>
