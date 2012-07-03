ff2e41345ac0d7c84771ec4b9065680b902836c0
srcspider <source.spider@gmail.com>
2012-07-03 18:36:38 +0300

    access update

--- +App/config/ibidem/access.php
+++ +App/config/ibidem/access.php
@@ -1,11 +1,4 @@
 <?php return array
 	(
-		'whitelist' => array
-			(
-				\app\A12n::guest() => array
-					(
-						\app\Protocol::instance()
-							->relays(['\ibidem\backend']),
-					),
-			)
+
 	);
