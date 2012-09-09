3afaaae99485f02c42234e79117f4649109a141e
srcspider <source.spider@gmail.com>
2012-09-10 02:11:25 +0300

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -3,7 +3,7 @@
 // This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
 // no effect on your application. :)
 
-// HowTo: order honeypot -n 'ibidem\backend'
+// HowTo: order honeypot -n 'mjolnir\backend'
 
 class Backend_Collection extends \mjolnir\backend\Backend_Collection { /** @return \mjolnir\backend\Backend_Collection */ static function instance() { return parent::instance(); } }
 class Context_Backend extends \mjolnir\backend\Context_Backend { /** @return \mjolnir\backend\Context_Backend */ static function instance() { return parent::instance(); } }
