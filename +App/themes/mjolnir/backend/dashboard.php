b832c305114a0dda3df758dfa457972237f3323e
srcspider <source.spider@gmail.com>
2013-04-01 12:34:03 +0300

    status information

--- +App/themes/mjolnir/backend/dashboard.php
+++ +App/themes/mjolnir/backend/dashboard.php
@@ -3,10 +3,87 @@
 
 	/* @var $theme ThemeView */
 
-	$versions = \app\CFS::config('version')
+	$versions = CFS::config('version');
+	$requrelist = CFS::config('mjolnir/require');
 ?>
 
 <h1>System Information</h1>
+
+<table class="table table-striped">
+	
+	<thead>
+		<tr>
+			<th>Status</th>
+			<th>Requirement</th>
+		</tr>
+	</thead>
+	
+	<?
+		$errors = 0;
+		$failed = 0;
+	?>
+	
+	<tbody>
+	
+		<? foreach ($requrelist as $ns => $requirements): ?>
+		
+			<? foreach ($requirements as $requirement => $tester): ?>
+				<?
+					try
+					{
+						 $statusinfo = $tester();
+					}
+					catch (\Exception $e)
+					{
+						\mjolnir\log_exception($e);
+						$statusinfo = 'untestable';
+					}
+
+					if (\is_array($statusinfo))
+					{
+						$status = \key($statusinfo);
+						$statushint = \current($statusinfo); 
+					}
+					else # non-array status
+					{
+						$status = $statushint = $statusinfo;
+					}
+
+					$statusclass = '';
+					switch ($status)
+					{
+						case 'untestable':
+							$statusclass = 'error';
+							$labelclass = 'label-error';
+							++$errors;
+							break;
+						case 'error':
+							$statusclass = 'error';
+							$labelclass = 'label-error';
+							++$errors;
+							break;
+						case 'failed':
+							$statusclass = 'warning';
+							$labelclass = 'label-warning';
+							++$failed;
+							break;
+						case 'available':
+							$labelclass = 'label-success';
+							break;
+					}
+				?>
+				<tr class="<?= $statusclass ?>">
+					<td><span class="label <?= $labelclass ?>"><?= $statushint ?></span></td>
+					<td><?= $requirement ?></td>
+				</tr>
+			<? endforeach; ?>
+			
+		<? endforeach; ?>
+				
+	</tbody>
+</table>
+
+<h2>Components</h2>
 
 <table class="table table-striped">
 	<thead>
@@ -19,7 +96,7 @@
 		<? foreach ($versions as $name => $version): ?>
 			<tr>
 				<td><?= $name ?></td>
-				<td><?= $version['major'].'.'.$version['minor'].(isset($version['tag']) && ! empty($version['tag']) ? '-'.$version['tag'] : '') ?></td>
+				<td><big><?= $version['major'].'.'.$version['minor'].(isset($version['tag']) && ! empty($version['tag']) ? '-'.$version['tag'] : '') ?></big></td>
 			</tr>
 		<? endforeach; ?>
 	</tbody>
